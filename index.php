842e194f5ace473bffd39e831080ff5aa4424938
Josh Endemann <jahend44@gmail.com>
2025-01-29 18:19:31 -0600

    Arrays

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
-<?= 
-$title = 'Brad\'s PHP Blog | Introduction to PHP';
-$heading = 'Introduction to PHP';
-$author = 'John Doe';
-$content = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting language that has revolutionized web development. With its simplicity, flexibility, and vast community support, PHP has become the backbone of countless dynamic websites and web applications.';
-?>
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $title ?></title>
-  </head>
-  <body>
-    <main>
-      <h1><?= $heading ?></h1>
-      <p>By: <?=$author?></p>
-      <p>
-        <?= $content ?>
-      </p>
-    </main>
-  </body>
-</html>
+$names = array("John", "Jane", "Joe", "James", "Jill");
+$numbers = [1, 2, 3, 4, 5];
+
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+   
+}
+
+// inspect($names);
+// inspect($numbers);
+
+
+// Fetch element from array
+// echo $names[2];
+// echo $numbers[4];
+
+// Add element to array
+$names[] = 'Jack';
+$numbers[] = 6;
+
+// Change at a specific position
+$numbers[3] = 44;
+
+// Remove element from array
+unset($names[1]);  // This removes the index as well
+
+$names = array_values($names); // This re-indexes the array
+
+inspect ($names);
+inspect ($numbers);
+?>
