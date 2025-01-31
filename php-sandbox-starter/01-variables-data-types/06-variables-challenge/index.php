5f85744b6b709d77b03a6d1acfeac6f78bf0df74
Josh Endemann <jahend44@gmail.com>
2025-01-30 20:45:43 -0600

    Add Files for course

---
+++ php-sandbox-starter/01-variables-data-types/06-variables-challenge/index.php
@@ -0,0 +1,24 @@
+<?= 
+$title = 'Brad\'s PHP Blog | Introduction to PHP';
+$heading = 'Introduction to PHP';
+$author = 'John Doe';
+$content = 'PHP (Hypertext Preprocessor) is a widely used server-side scripting language that has revolutionized web development. With its simplicity, flexibility, and vast community support, PHP has become the backbone of countless dynamic websites and web applications.';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $title ?></title>
+  </head>
+  <body>
+    <main>
+      <h1><?= $heading ?></h1>
+      <p>By: <?=$author?></p>
+      <p>
+        <?= $content ?>
+      </p>
+    </main>
+  </body>
+</html>
